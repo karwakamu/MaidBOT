b94e5a68b993b22d3d4c0992a6a4b8b41a3afbaa
Tuomas <tuomas.kotilainen@denovo.fi>
2019-02-20 14:28:20 +0200

    lisätty dotenv jotta voi käyttää .env filuja omalla koneella

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
+require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const cleaning = require('./cleaning');
 
-bot_secret_token = "NTQ1OTUxNjYxNjcyMzY2MTEw.D07SUw.ViNptuZaqZigtvbit4fdDH9Gee8"
-client.login(bot_secret_token)
-
-//client.login(process.env.bot_secret_token);
+client.login(process.env.bot_secret_token);
 
 client.on('ready', () => {
 	console.log("connected");	
